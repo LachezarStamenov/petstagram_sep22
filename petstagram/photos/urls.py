faec629398f6a25dccfb067a5bb21d223f5f5e4c
Lachezar <lachezar.stamenov@abv.bg>
2022-09-28 19:37:35 +0300

    photos views added

--- petstagram/photos/urls.py
+++ petstagram/photos/urls.py
@@ -1,4 +1,12 @@
-from django.urls import path
+from django.urls import path, include
+
+from petstagram.photos.views import add_photo, details_photo, edit_photo
 
 urlpatterns = [
+    path('add/', add_photo, name='add photo'),
+    path('<int:pk>/', include([
+        path('', details_photo,name='detail photo'),
+        path('edit/', edit_photo, name='edit_photo'),
+    ])),
+
 ]
