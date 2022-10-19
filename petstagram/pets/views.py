33287bc4bf85a054f7de9a16b84590d3fef682e7
Lachezar <lachezar.stamenov@abv.bg>
2022-10-19 15:34:28 +0300

    delete pet form added

--- petstagram/pets/views.py
+++ petstagram/pets/views.py
@@ -1,12 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from petstagram.core.photo_utils import apply_likes_count, apply_user_liked_photo
+from petstagram.pets.forms import PetForm
 from petstagram.pets.models import Pet
 from petstagram.pets.utils import get_pet_by_name_and_username
 
 
 def add_pet(request):
-    return render(request, 'pets/pet-add-page.html')
+    form = PetForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('details user', pk=1)
+    context = {'form': form}
+    return render(request, 'pets/pet-add-page.html', context)
 
 
 def delete_pet(request, username, pet_slug):
@@ -26,6 +32,15 @@ def details_pet(request, username, pet_slug):
     return render(request, 'pets/pet-details-page.html', context)
 
 
-def edit_pet(request, username, pet_name):
-    return render(request, 'pets/pet-edit-page.html')
+def edit_pet(request, username, pet_slug):
+    pet = Pet.objects.get(slug=pet_slug)
+    if request.method == "GET":
+        form = PetForm(instance=pet, initial=pet.__dict__)
+    else:
+        form = PetForm(request.POST, instance=pet)
+        if form.is_valid():
+            form.save()
+            return redirect('details pet', username, pet_slug)
+    context = {'form': form}
+    return render(request, 'pets/pet-edit-page.html', context)
 
